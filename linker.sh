de3b54a6a2f4f6d450a79cf35536fedda0a4cfdd
skylineone044 <tokodioli@gmail.com>
2020-03-14 19:13:06 +0100

    a

--- linker.sh
+++ linker.sh
@@ -16,3 +16,4 @@ mkdir ~/.local/share/plasma/look-and-feel
 ln -rs look-and-feel/Stardust ~/.local/share/plasma/look-and-feel
 
 printf "\nkvantum theme is done manually, through kvantum-manager"
+printf "\n Done."
